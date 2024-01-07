f91d2e61d45544b47b24ce0c5c757ea95833a15f
marcelkornblum <me@marcelkornblum.com>
2024-01-07 02:17:36 +0000

    venv

--- .p10k.zsh
+++ .p10k.zsh
@@ -53,15 +53,16 @@
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
-    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
+    # asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    venv
     # anaconda                # conda environment (https://conda.io/)
     # pyenv                   # python environment (https://github.com/pyenv/pyenv)
     # goenv                   # go environment (https://github.com/syndbg/goenv)
     # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
     # nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
     # nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
-    # node_version          # node.js version
+    node_version          # node.js version
     # go_version            # go version (https://golang.org)
     # rust_version          # rustc version (https://www.rust-lang.org)
     # dotnet_version        # .NET version (https://dotnet.microsoft.com)
@@ -1834,6 +1835,19 @@
   }
   typeset -g POWERLEVEL9K_GITDIR_FOREGROUND=7
   typeset -g POWERLEVEL9K_GITDIR_BACKGROUND=4
+
+  function prompt_venv() {
+    if
+      [ -z "${VIRTUAL_ENV}" ]
+    then
+      # no active VIRTUAL_ENV
+    else
+      version=$( python --version)
+      p10k segment -i "${version:7:4} "
+    fi
+  }
+  typeset -g POWERLEVEL9K_VENV_FOREGROUND=226
+  typeset -g POWERLEVEL9K_VENV_BACKGROUND=27
 
   function prompt_emoji() {
     # declares an array with the emojis we want to support
